6d57d54c537b5daaaeab04a40e92fcb7ad04ae74
Federico Reale <cereale.federico@gmail.com>
2018-12-06 16:09:06 +0100

    Added TimeController Script

---
+++ Sona/Assets/Scripts/Controllers/TimeController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeController : MonoBehaviour {
+
+    static float slowDeltaTime = 0.1f;
+    static bool slow = false;
+
+    public static float GetDelTaTime() {
+
+        if (slow)
+        {
+            return slowDeltaTime * Time.deltaTime;
+        }
+        else {
+            return Time.deltaTime;
+        }
+    }
+
+    public static void changeTime() {
+        slow = !slow;
+    }
+
+}
