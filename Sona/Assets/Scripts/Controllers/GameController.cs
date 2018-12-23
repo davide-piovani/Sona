fa9d66c2396e67a2f6975aa6e962a6b67b6c9e7f
Gabriele Iannone <g.iannone001@gmail.com>
2018-12-23 18:22:47 +0100

    First level v1, powerbar and interactable object
    Added first version of first level, character circle powerbar and an image of character, implemented interactable object system too

--- Sona/Assets/Scripts/Controllers/GameController.cs
+++ Sona/Assets/Scripts/Controllers/GameController.cs
@@ -2,17 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.UI;
 
 public class GameController : MonoBehaviour {
 
+    public Transform LoadingBar;
     private float fullWidth;
-    [SerializeField] RectTransform powerLevelIndicator;
+    [SerializeField] float powerLevelIndicator;
     [SerializeField] Player[] scenePlayers;
 
     // Use this for initialization
     void Start () {
-        RectTransform powerLevelIndicatorContainer = (UnityEngine.RectTransform) powerLevelIndicator.parent;
-        fullWidth = powerLevelIndicatorContainer.rect.width;
+        //RectTransform powerLevelIndicatorContainer = (UnityEngine.RectTransform) powerLevelIndicator.parent;
+
+        //fullWidth = powerLevelIndicatorContainer.rect.width;
 	}
 
     private void Update() {
@@ -21,9 +24,39 @@ public class GameController : MonoBehaviour {
         }
     }
 
-    public void UpdatePowerLevelIndicator(float level){
-        float newWidth = fullWidth * level;
-        powerLevelIndicator.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, newWidth);
+    //public void UpdatePowerLevelIndicator(Player player)
+    public void UpdatePowerLevelIndicator(float level)
+    {
+        /*
+        if (player.powerActive)
+        {
+            player.powerTimeLeft -= Time.deltaTime;
+
+            if (player.powerTimeLeft <= 0) player.PowerToggle(false);
+        }
+        else
+        {
+            if (player.powerTimeLeft < player.powerDuration)
+            {
+                player.powerTimeLeft += Time.deltaTime * player.rechargeSpeed;
+            }
+            else
+            {
+                player.powerTimeLeft = player.powerDuration;
+            }
+        }
+        
+        if (powerLevelIndicator < 100)
+        {
+            powerLevelIndicator += level;
+        }
+        else
+        {
+            powerLevelIndicator -= level;
+        }
+        */
+        //powerLevelIndicator =  player.powerTimeLeft / player.powerDuration;
+        LoadingBar.GetComponent<Image>().fillAmount = level;
     }
 
     public Player[] GetScenePlayers() { return scenePlayers; }
--- Sona/Assets/Scripts/Utils/Interactable.cs
+++ Sona/Assets/Scripts/Utils/Interactable.cs
@@ -1,6 +1,7 @@
 ﻿
 using System;
 using UnityEngine;
+using TMPro;
 
 public class Interactable : MonoBehaviour {
 
@@ -10,28 +11,52 @@ public class Interactable : MonoBehaviour {
     bool isFocus = false;
     bool hasInteracted = false;
 
+    public GameObject playerObject;
+    TextMeshPro text;
+
     Transform player;
 
     public Transform interactionTransform;
 
+    /**
+     * virtual method
+     */
     public virtual void Interact()
     {
 
     }
 
+
+    public void Start ()
+    {
+        text = GetComponentInChildren<TextMeshPro>();
+        player = playerObject.transform;
+    }
+
+
     public void Update()
     {
+        float distance = Vector3.Distance(player.position, interactionTransform.position);
         if (isFocus && !hasInteracted)
         {
-            float distance = Vector3.Distance(player.position, interactionTransform.position);
             if (distance <= radius)
             {
                 Interact();
                 hasInteracted = true;
             }
         }
+
+        if (distance <= radius)
+        {
+            ShowTooltip();
+        }
+        else
+        {
+            HideTooltip();
+        }
     }
 
+    //focus method
     public void OnFocused(Transform playerTransform)
     {
         isFocus = true;
@@ -39,6 +64,9 @@ public class Interactable : MonoBehaviour {
         hasInteracted = false;
     }
 
+    /**
+     * Draw interaction radius
+     */
     private void OnDrawGizmosSelected()
     {
 
@@ -51,9 +79,29 @@ public class Interactable : MonoBehaviour {
         Gizmos.DrawWireSphere(interactionTransform.position, radius);
     }
 
+    //defocus method
     internal void onDefocus()
     {
         isFocus = false;
         hasInteracted = false;
+    }
+
+    /**
+     * This method is used to show the text and to position it on the player.
+     */ 
+    void ShowTooltip()
+    {
+        var CamPos = Camera.main.transform.position + Camera.main.transform.forward;
+        text.enabled = true;
+        text.transform.position = CamPos;
+        text.transform.localScale = Vector3.one * 0.025f;
+    }
+
+    /**
+    * This method is used to hide the text
+    */
+    void HideTooltip()
+    {
+        text.enabled = false;
     }
 }
