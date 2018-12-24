0cccf196d17a6f94cc26e4708de83b66df44cfa4
Federico Reale <cereale.federico@gmail.com>
2018-12-24 18:05:38 +0100

    Elevator system of Intermediate level added

--- Sona/Assets/Scripts/Controllers/LevelControllers/Intermediate Level/PlatformController.cs
+++ Sona/Assets/Scripts/Controllers/LevelControllers/Intermediate Level/PlatformController.cs
@@ -4,79 +4,184 @@ using UnityEngine;
 
 public class PlatformController : MonoBehaviour {
 
-    public float maxHeight;
-    public float minHeight;
-    public float speed;
+    public ElevatorDoor bottomDoor;
+    public ElevatorDoor topDoor;
+    public GameObject bottomDisplay;
+    public GameObject topDisplay;
+    public GameObject player;
+    public float minDist;
+    public float minDistToCloseDoor;
+    PlatformMovement platform;
+    bool bottomDisplayActive = false;
+    bool topDisplayActive = false;
 
-    bool isMoving = false;
-    bool up = false;
-    float x;
-    float z;
-
-    GameObject player;
-
-    void Start()
-    {
-        x = gameObject.transform.localPosition.x;
-        z = gameObject.transform.localPosition.z;
-    }
+    int state = 0;
+    
+    void Start () {
+        platform = GetComponentInChildren<PlatformMovement>();
+	}
+	
+	void Update () {
 
 
-    void Update () {
-
-        if (!isMoving & Input.GetKeyDown(KeyCode.B)) {
-            ChangeDirection();
-            isMoving = true;
-        }
-
-        if (isMoving){
-            Move(up, player);
-        }
-    }
-
-    void Move(bool cond, GameObject player) {
-
-        if (cond & gameObject.transform.localPosition.y <= maxHeight)
+        if ((player.transform.position - bottomDisplay.transform.position).magnitude <= minDist)
         {
-            gameObject.transform.Translate(Vector3.up * speed * TimeController.GetDelTaTime());
-            player.transform.Translate(Vector3.up * speed * TimeController.GetDelTaTime());
+            bottomDisplayActive = true;
         }
-        else if (cond & gameObject.transform.localPosition.y >= maxHeight)
+        else {
+            bottomDisplayActive = false;
+        }
+
+        if ((player.transform.position - topDisplay.transform.position).magnitude <= minDist)
         {
-            gameObject.transform.localPosition = new Vector3(x ,maxHeight, z);
-            isMoving = false;
+            topDisplayActive = true;
         }
-        else if (!cond & gameObject.transform.localPosition.y >= minHeight)
+        else
         {
-            gameObject.transform.Translate(-Vector3.up * speed * TimeController.GetDelTaTime());
-            player.transform.Translate(-Vector3.up * speed * TimeController.GetDelTaTime());
+            topDisplayActive = false;
         }
-        else if (!cond & gameObject.transform.localPosition.y <= minHeight)
-        {
-            gameObject.transform.localPosition = new Vector3(x, minHeight, z);
-            isMoving = false;
-        }
+
+        ChangeState();
+        
 
     }
 
-    void ChangeDirection() {
-        up = !up;
-    }
+    void ChangeState() {
 
-
-    void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.CompareTag("Player")) {
-            player = collision.gameObject;
-        }
-    }
-
-    void OnCollisionExit(Collision collision)
-    {
-        if (collision.gameObject.CompareTag("Player"))
+        if (state == 0)
         {
-            player = null;
+
+            if (Input.GetKeyDown(KeyCode.B))
+            {
+
+                if (bottomDisplayActive)
+                {
+                    platform.ActiveDeActivePlatform(true);
+                    state = 1;
+                }
+                else if (topDisplayActive)
+                {
+                    platform.ActiveDeActivePlatform(true);
+                    state = 10;
+                }
+                else { }
+
+            }
+        }
+        else if (state == 1) {
+
+            if (platform.isTop())
+            {
+                platform.MovePlatform();
+                state = 2;
+            }
+            else if (platform.isBottom())
+            {
+                bottomDoor.SlideDoor();
+                state = 3;
+            }
+            else { }
+
+        }
+        else if (state == 2)
+        {
+
+            if (platform.isBottom()){
+                bottomDoor.SlideDoor();
+                state = 3;
+            }
+
+        }
+        else if (state == 3)
+        {
+
+            if (!bottomDisplayActive & !platform.isPlayerOnPlatform() & (player.transform.position - bottomDisplay.transform.position).magnitude > minDistToCloseDoor) {
+                bottomDoor.SlideDoor();
+                state = 4;
+            }
+            else if (Input.GetKeyDown(KeyCode.B) & platform.isPlayerOnPlatform() & bottomDoor.isOpen()) {
+                bottomDoor.SlideDoor();
+                state = 5;
+            }
+            else { }
+
+        }
+        else if (state == 4)
+        {
+            if (bottomDoor.isClose()){
+                platform.ActiveDeActivePlatform(false);
+                state = 0;
+            }
+        }
+        else if (state == 5)
+        {
+
+            if (bottomDoor.isClose()) {
+                platform.MovePlatform();
+                state = 6;
+            }
+
+        }
+        else if (state == 6)
+        {
+
+            if (platform.isTop()) {
+                topDoor.SlideDoor();
+                state = 7;
+            }
+
+        }
+        else if (state == 7)
+        {
+
+            if (!topDisplayActive & !platform.isPlayerOnPlatform() & (player.transform.position - topDisplay.transform.position).magnitude > minDistToCloseDoor)
+            {
+                topDoor.SlideDoor();
+                state = 9;
+            }
+            else if (Input.GetKeyDown(KeyCode.B) & platform.isPlayerOnPlatform() & topDoor.isOpen())
+            {
+                topDoor.SlideDoor();
+                state = 8;
+            }
+            else { }
+
+        }
+        else if (state == 8)
+        {
+
+            if (topDoor.isClose()) {
+                platform.MovePlatform();
+                state = 2;
+            }
+
+        }
+        else if (state == 9)
+        {
+
+            if (topDoor.isClose())
+            {
+                platform.ActiveDeActivePlatform(false);
+                state = 0;
+            }
+
+        }
+        else if (state == 10)
+        {
+
+            if (platform.isBottom())
+            {
+                platform.MovePlatform();
+                state = 6;
+            }
+            else if (platform.isTop())
+            {
+                topDoor.SlideDoor();
+                state = 7;
+            }
+            else { }
+
         }
     }
-   
+    
 }
