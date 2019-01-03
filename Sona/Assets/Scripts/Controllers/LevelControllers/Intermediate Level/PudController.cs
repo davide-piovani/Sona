6e00c625d05b79d9156a3abae78c236b3c8b38d7
Federico Reale <cereale.federico@gmail.com>
2019-01-03 01:57:36 +0100

    Added poison liquid and other stuff

---
+++ Sona/Assets/Scripts/Controllers/LevelControllers/Intermediate Level/PudController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PudController : MonoBehaviour {
+
+    Animator _animatorController;
+    float deltaTime;
+    
+    void Start()
+    {
+        _animatorController = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+
+        deltaTime = TimeController.GetDelTaTime();
+        _animatorController.SetFloat("speed", deltaTime);
+
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("MORTOOOOOOOOOOO " + gameObject.name);
+        }
+    }
+}
