678d98d8ff06a11620200e999299ef7a18f7d6e4
Federico Reale <cereale.federico@gmail.com>
2019-01-03 16:38:32 +0100

    KeyDoor Controller added

---
+++ Sona/Assets/Scripts/Controllers/LevelControllers/Intermediate Level/KeyScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyScript : MonoBehaviour {
+
+    DisplayActivator _display;
+    public KeyDoorController keyDoorController;
+
+    void Start() {
+        _display = GetComponent<DisplayActivator>();
+    }
+
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.B) & _display.IsActive())
+        {
+            keyDoorController.KeyTaken(_display.GetPlayer().name);
+            gameObject.SetActive(false);
+        }
+    }
+
+}
