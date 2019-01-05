e2945c3a0dae8caf78cc2572769d3739b3b8c3a5
Luca <luca.sartor1994@gmail.com>
2019-01-05 19:55:58 +0100

    Added elements in level 3, added walk button in Input Manager

---
+++ Sona/Assets/Scripts/Controllers/LevelControllers/Level3Controllers/DuctController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ApplicationConstants;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class DuctController : MonoBehaviour {
+
+    private bool active = true;
+    private bool operating = false;
+    private Level3Manager manager;
+
+    void Start(){
+        manager = FindObjectOfType<Level3Manager>();
+    }
+
+    void OnTriggerStay (Collider other){
+        //Collider other;
+
+        //other = collision.collider;
+        if (other.CompareTag(PlayersConstants.playerTag) && active){
+            Player player = other.gameObject.GetComponent<Player>();
+            if (player.GetPlayerType() == PlayerType.Charlie){
+                manager.ShowMessage("Crouch", 0);
+                //print("DUCT CONTROLLER: considering collision");
+                if (CrossPlatformInputManager.GetButtonDown(PlayersConstants.interactButton)){
+                    manager.EraseMessage();
+                    operating = true;
+                    print("DUCTCONTROLLER: interacting");
+                }
+                if(operating){
+                    print ("DUCTCONTROLLER: Operating");
+                    Charlie charlie = player as Charlie;
+                    operating = !(charlie.Crouch());
+                    if (!operating){
+                    active = false;
+                    }
+                }
+            } else {
+                manager.ShowMessage("Only Charlie can pass through here", 0);
+            }
+        }
+    }
+
+    void OnTriggerExit (Collider other){
+        if (other.CompareTag(PlayersConstants.playerTag)){
+            manager.EraseMessage();
+            Player player = other.gameObject.GetComponent<Player>();
+            if (player.GetPlayerType() == PlayerType.Charlie){
+                Charlie charlie = player as Charlie;
+                charlie.Stand();
+            }
+            operating = false;
+            active = true;
+        }
+    }
+}
--- Sona/Assets/Scripts/Controllers/LevelControllers/Level3Controllers/Torch.cs
+++ Sona/Assets/Scripts/Controllers/LevelControllers/Level3Controllers/Torch.cs
@@ -4,19 +4,30 @@ using UnityEngine;
 
 public class Torch : MonoBehaviour {
 
+    public GameObject cam;
+
     private Light light;
     private Animator anim;
     private Renderer renderer;
     private float base_intensity;
+    private Vector3 w_position;
+    private Vector3 i_position;
+    private Transform tr;
+    private Transform parent;
 
     // Use this for initialization
     void Start () {
         Transform main;
 
+        tr = GetComponent<Transform>();
+        parent = tr.parent;
         light = GetComponentInChildren<Light>();
         renderer = GetComponent<Renderer>();
-        main = GetComponent<Transform>().parent.parent;
+        main = parent.parent;
         anim = main.gameObject.GetComponent<Animator> ();
+
+        w_position = new Vector3 (0.00153f, -0.00114f, 0.00025f);
+        i_position = new Vector3 (0.00218f, -0.00083f, -0.00063f);
 
         if (light == null || anim == null || renderer == null){
             print ("Missing fundamental component");
@@ -28,12 +39,32 @@ public class Torch : MonoBehaviour {
     // Update is called once per frame
     void Update () {
         if (anim.GetBool ("isWalking")){
+            tr.localPosition = w_position;
             light.intensity = base_intensity;
             renderer.enabled = true;
-            
+            //RotateLight();
+        } else if (anim.GetBool("isIdle")){
+            tr.localPosition = i_position;
+            light.intensity = base_intensity;
+            renderer.enabled = true;
+            //RotateLight();
         } else {
             light.intensity = 0f;
             renderer.enabled = false;
         }
     }
+
+    /*void RotateLight (){
+        float angle;
+        Vector3 start = parent.right;
+        Vector3 dest = cam.transform.forward;
+
+        dest[2] = 0;
+
+        angle = Vector3.SignedAngle(start, dest, Vector3.up);
+        if (Mathf.Abs(angle) > 30){
+            angle = 30 * Mathf.Sign(angle);
+        }
+        tr.localRotation = Quaternion.Euler(0, 0, 180 + angle);
+    }*/
 }
