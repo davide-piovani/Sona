1f0fde2a8f0af939007cc46224dcda7306370728
Federico Reale <cereale.federico@gmail.com>
2019-01-26 23:30:39 +0100

    partial fix

---
+++ Sona/Assets/Scripts/Controllers/LevelControllers/Intermediate Level/Canvas Scripts/ShowK.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShowK : MonoBehaviour {
+
+    public GameObject _door;
+    public Sprite _key;
+
+    LockedDoor _lockDoor;
+    
+    Image _image;
+    GameController _gameController;
+
+    void Start()
+    {
+        _lockDoor = _door.GetComponentInChildren<LockedDoor>();
+        _gameController = FindObjectOfType<GameController>();
+        _image = GetComponent<Image>();
+        _image.enabled = false;
+    }
+
+    void Update()
+    {
+        if (!_lockDoor.isKeyUsed() & _lockDoor.isKeyOwned() & _gameController.GetActivePlayer().gameObject.name.Equals(_lockDoor.KeyOwner()))
+        {
+            _image.sprite = _key;
+            _image.enabled = true;
+        }
+        else
+        {
+            _image.enabled = false;
+        }
+    }
+}
